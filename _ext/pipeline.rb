2b8209af8d06837850ab3a3882c0dddef698950a
Gunnar Morling <gunnar.morling@googlemail.com>
2013-05-28 01:09:42 +0200

    #25 Adding news feed

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -2,8 +2,10 @@ require 'bootstrap-sass'
 
 Awestruct::Extensions::Pipeline.new do
   helper Awestruct::Extensions::GoogleAnalytics
-  # extension Awestruct::Extensions::Posts.new '/news'
+  extension Awestruct::Extensions::Posts.new( '/news', :posts )
+  extension Awestruct::Extensions::Paginator.new( :posts, '/news/index', :per_page=>5 )
+  extension Awestruct::Extensions::Tagger.new( :posts, '/news/index', '/news/tags', :per_page=>5 )
   # extension Awestruct::Extensions::Indexifier.new
   # Indexifier *must* come before Atomizer
-  # extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
+  extension Awestruct::Extensions::Atomizer.new( :posts, '/news.atom', :feed_title=>'MapStruct News' )
 end
---
+++ news/2013-05-22-website-live.md
@@ -0,0 +1,8 @@
+---
+title: mapstruct.org has landed!
+author: Gunnar Morling
+layout: news
+tags: [web-site]
+---
+
+MapStruct has now its own web site, [mapstruct.org](http://mapstruct.org)!
