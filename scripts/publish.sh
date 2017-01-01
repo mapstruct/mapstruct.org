7cf8454694d6fc114e1cd54801758dde3740dfb3
Gunnar Morling <gunnar.morling@googlemail.com>
2017-01-02 00:11:57 +0100

    Updating publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,9 +1,8 @@
 rm -rf public/*
 hugo
-rm -rf public/pages
+rm -rf public/page
 rm public/index.xml
 rm public/documentation/index.xml
 rm public/community/index.xml
 rm public/development/index.xml
-git add public
-git commit . -m "Publishing to gh-pages (publish.sh)"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
