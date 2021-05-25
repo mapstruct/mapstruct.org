6ae62665e20843f9e0559da09e2212ab20c974a8
Raimund Klein <770876+Chessray@users.noreply.github.com>
2021-05-25 10:13:12 +0100

    News item and documentation for MapStruct Spring Extensions 0.0.3.

--- static/documentation/spring-extensions/api/extensions/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.mapstruct.extensions.spring.converter","l":"ConversionServiceAdapterDescriptor"},{"p":"org.mapstruct.extensions.spring.converter","l":"ConversionServiceAdapterGenerator"},{"p":"org.mapstruct.extensions.spring.converter","l":"ConverterMapperProcessor"}]
