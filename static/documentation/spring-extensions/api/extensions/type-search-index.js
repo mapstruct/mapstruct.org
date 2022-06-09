1cc4aedc5bfc295286706abc0bf7e4656520f3fa
Raimund Klein <770876+Chessray@users.noreply.github.com>
2022-06-09 10:28:30 +0100

    Extensions Release 0.1.2 - news item and updated documentation.

---
+++ static/documentation/spring-extensions/api/extensions/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.mapstruct.extensions.spring.converter","l":"ConversionServiceAdapterDescriptor"},{"p":"org.mapstruct.extensions.spring.converter","l":"ConversionServiceAdapterGenerator"},{"p":"org.mapstruct.extensions.spring.converter","l":"ConverterMapperProcessor"}]
